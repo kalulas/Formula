59decbbae3bc581c2391429437e7ddf44ac017ec
edwardchen <499485532@qq.com>
2024-02-03 22:28:31 +0800

    feat: create formula binding and console example

---
+++ Formula.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using TreeSitter.Bindings;
+
+if (args.Length < 2 || args[0] != "-file")
+{
+    Console.WriteLine("Invalid arguments. Please use -file followed by the file path.");
+    return;
+}
+
+var filepath = args[1];
+var sourceCode = File.ReadAllText(filepath);
+var parser = new TSParser();
+parser.set_language(Language.Formula);
+var tree = parser.parse_string(null, sourceCode);
+if (tree == null)
+{
+    Console.WriteLine($"Error. Parse file '{filepath}' to tree failed.");
+    return;
+}
+
+// Console.WriteLine(tree.root_node().text(sourceCode));
+Console.WriteLine(tree.root_node().to_string());
---
+++ TreeSitter.Bindings/Language.cs
@@ -0,0 +1,18 @@
+﻿#region File Header
+// Filename: Language.cs
+// Author: Kalulas
+// Create: 2024-2-3
+// Description:
+#endregion
+
+using System.Runtime.InteropServices;
+
+namespace TreeSitter.Bindings;
+
+public static class Language
+{
+    [DllImport("tree-sitter-formula.dll", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr tree_sitter_formula();
+
+    public static readonly TSLanguage Formula = new TSLanguage(tree_sitter_formula());
+}
